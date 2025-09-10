6f189cc2a83f7de4dbd24d1f70b652873e595698
Iswahyudi Yudi <khalidahmadnaufal26@gmail.com>
2025-09-10 12:13:58 +0900

    7th commit

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 // Toggle Class Active
-const navbarNav = document.querySelector('.navbar-nav');
+const navbarNav = document.querySelector(".navbar-nav");
 // Ketika Menu di-klik
-document.querySelector('#menu').onclick = () => {
-    navbarNav.classList.toggle('active');
+document.querySelector("#menu").onclick = () => {
+  navbarNav.classList.toggle("active");
 };
 // klik di luar sidebar untuk menghilangka  sidebar
-const menu = document.querySelector('#menu');
+const menu = document.querySelector("#menu");
 
-document.addEventListener('click', function(e){
-    if(!menu.contains(e.target) && !navbarNav.contains(e.target)){
-        navbarNav.classList.remove('active');
-    }
+document.addEventListener("click", function (e) {
+  if (!menu.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
 });
